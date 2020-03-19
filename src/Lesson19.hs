42d48035de81fd1d7fb70e8de429d5a8b711a438
py0n <py0n@users.noreply.github.com>
2020-03-19 12:54:22 +0900

    add: Lesson19のOrganを写経する

--- src/Lesson19.hs
+++ src/Lesson19.hs
@@ -1,7 +1,57 @@
 module Lesson19
   ( numOrZero
+  , organCatalog
+  , processRequest
   )
 where
+
+import           Data.Map                      as Map
+
+-- Lesson 19
+data Organ = Heart | Brain | Kidney | Spleen deriving (Show, Eq)
+
+organCatalog :: Map Int Organ
+organCatalog = Map.fromList
+  [ (2 , Heart)
+  , (7 , Heart)
+  , (13, Brain)
+  , (14, Spleen)
+  , (21, Spleen)
+  , (24, Kidney)
+  ]
+
+data Container = Vat Organ | Cooler Organ | Bag Organ
+
+instance Show Container where
+  show (Bag    o) = show o ++ " in a bag"
+  show (Cooler o) = show o ++ " in a cooler"
+  show (Vat    o) = show o ++ " in a vat"
+
+data Location = Lab | Kitchen | Bathroom deriving (Show)
+
+organToContainer :: Organ -> Container
+organToContainer Brain = Vat Brain
+organToContainer Heart = Cooler Heart
+organToContainer o     = Bag o
+
+placeInLocation :: Container -> (Location, Container)
+placeInLocation (Vat    a) = (Lab, Vat a)
+placeInLocation (Cooler a) = (Lab, Cooler a)
+placeInLocation (Bag    a) = (Kitchen, Bag a)
+
+process :: Organ -> (Location, Container)
+process o = placeInLocation (organToContainer o)
+
+report :: (Location, Container) -> String
+report (l, c) = show c ++ " in the " ++ show l
+
+processRequest :: Int -> Map.Map Int Organ -> String
+processRequest id catalog = processAndReport organ
+  where organ = Map.lookup id catalog
+
+processAndReport :: Maybe Organ -> String
+processAndReport Nothing      = "error, id not found"
+processAndReport (Just organ) = report (process organ)
 
 -- Quick Check 19-2
 numOrZero :: Maybe Int -> Int
