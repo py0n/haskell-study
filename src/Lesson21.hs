3d0cc4b57ab090898d9e60932b66af88aa3ac4c6
py0n <py0n@users.noreply.github.com>
2020-03-23 18:59:41 +0900

    add: Lesson21.3のmaybeMainを写経する

--- src/Lesson21.hs
+++ src/Lesson21.hs
@@ -1,8 +1,11 @@
 module Lesson21
   ( comparePizza
   , helloPerson
+  , maybeMain
   )
 where
+
+import qualified Data.Map                      as Map
 
 -- Lesson 21.2
 createHello :: String -> String
@@ -50,3 +53,20 @@ comparePizza = do
   let pizza2      = (read size2, read cost2)
   let betterPizza = comparePizzaCost pizza1 pizza2
   putStrLn (describePizza betterPizza)
+
+costData :: Map.Map Int Double
+costData = Map.fromList [(1, 1000.0), (2, 2000.0)]
+
+sizeData :: Map.Map Int Double
+sizeData = Map.fromList [(1, 18.0), (2, 26.0)]
+
+maybeMain :: Maybe String
+maybeMain = do
+  size1 <- Map.lookup 1 sizeData
+  cost1 <- Map.lookup 1 costData
+  size2 <- Map.lookup 2 sizeData
+  cost2 <- Map.lookup 2 costData
+  let pizza1      = (size1, cost1)
+  let pizza2      = (size2, cost2)
+  let betterPizza = comparePizzaCost pizza1 pizza2
+  return (describePizza betterPizza)
