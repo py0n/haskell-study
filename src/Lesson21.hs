10ee4959272495acdac726efc2db35c94709fa62
py0n <py0n@users.noreply.github.com>
2020-03-23 18:52:51 +0900

    add: Lesson21.3のcomparePizzaを写経する

--- src/Lesson21.hs
+++ src/Lesson21.hs
@@ -1,5 +1,6 @@
 module Lesson21
-  ( helloPerson
+  ( comparePizza
+  , helloPerson
   )
 where
 
@@ -13,3 +14,39 @@ helloPerson = do
   name <- getLine
   let statement = createHello name
   putStrLn statement
+
+-- Lesson 21.3
+areaGivenDiameter :: Double -> Double
+areaGivenDiameter size = pi * (size / 2) ^ 2
+
+type Pizza = (Double, Double)
+
+costPerCentimeter :: Pizza -> Double
+costPerCentimeter (size, cost) = cost / areaGivenDiameter size
+
+comparePizzaCost :: Pizza -> Pizza -> Pizza
+comparePizzaCost p1 p2 =
+  if costPerCentimeter p1 < costPerCentimeter p2 then p1 else p2
+
+describePizza :: Pizza -> String
+describePizza (size, cost) =
+  "The "
+    ++ show size
+    ++ " pizza is cheaper at "
+    ++ show (costPerCentimeter (size, cost))
+    ++ " per square centimeters"
+
+comparePizza :: IO ()
+comparePizza = do
+  putStrLn "What is the size of pizza 1"
+  size1 <- getLine
+  putStrLn "What is the cost of pizza !"
+  cost1 <- getLine
+  putStrLn "What is the size of pizza 2"
+  size2 <- getLine
+  putStrLn "What is the cost of pizza !"
+  cost2 <- getLine
+  let pizza1      = (read size1, read cost1)
+  let pizza2      = (read size2, read cost2)
+  let betterPizza = comparePizzaCost pizza1 pizza2
+  putStrLn (describePizza betterPizza)
