bff68aceb476b30069351951482fe7d8933152d7
py0n <py0n@users.noreply.github.com>
2020-04-01 19:23:52 +0900

    update: Q21-2のプログラムを實行できるやうに修正する.

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ main :: IO ()
 main = do
   args <- E.getArgs
   case head args of
+    "answerFib"         -> L21.answerFib
     "mainComparePizzas" -> L21.mainComparePizzas
     "mainDie"           -> L21.mainDie
     "mainHelloPerson"   -> L21.mainHelloPerson
